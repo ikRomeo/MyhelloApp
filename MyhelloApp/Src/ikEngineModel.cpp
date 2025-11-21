88e53b8ec864f6d03fa4430c9bd81b91ecf8ed7e
ikRomeo <ikromeo93@gmail.com>
2025-11-21 13:46:52 +0100

    Auto-commit: 2025-11-21 13:46:52

--- MyhelloApp/Src/ikEngineModel.cpp
+++ MyhelloApp/Src/ikEngineModel.cpp
@@ -25,27 +25,16 @@ namespace std {
 
 namespace ikE {
 
-	/*in the constructor 
-	  the constructor has 2 arguments and initlizer list an argument
-	  Member initializer list (:IkeDevice(device))
-	  This initializes the reference IkeDevice(the Vulkan device wrapper) directly before the constructor body runs
-	  createVertexBuffer(vertices)
-	  immediately calls the private function that uploads the vertex data to GPU memory By doing this in the constructor
-	  we ensure that once an ikEngineModel object exists,the GPU buffer is ready 
-	  meaning when we create a model, it automatically builds its vertex buffer and uploads data to the GPU
-	  Notice that the initilizer has IkeDevice which is the reference(&) instance of IkeDeviceEngine placed in the
-	  private section of the class ikEngineModel
-	  inside the initialize list function we call the createVertexBuffers() with vertices as argument
-	  */
+	
+
 	ikEngineModel::ikEngineModel(IkeDeviceEngine &device, const ikEngineModel::Builder& builder) : IkeDevice(device) {
 		createVertexBuffers(builder.vertices);
 		createIndexBuffers(builder.indices);
 
 	}
-	/* in the destructor
-	   we call vkDestroyBuffer with 3 arguments (IkeDevice which is the initilizer .device() function and vertexBuffer of type VkBuffer and nullptr) this frees vkhandle
-	   we call vkFreeMemory  with 3 arguments (the vertexBufferMemory of type VkDeviceMemory and nullptr) this frees the memory that was allocated for the buffer
-	   */
+
+
+
 	ikEngineModel::~ikEngineModel() {}
 
 	std::unique_ptr<ikEngineModel> ikEngineModel::createModelFromFile(IkeDeviceEngine& device, const std::string& filepath) {
@@ -60,26 +49,7 @@ namespace ikE {
 
 
 
-	/*
-	 createVertexBuffers we access the struct which is in the hpp file
-	 and put it in a dynamic array that is a vector so that we can access it's size
-	 then reference (&) it with an instance vertices
-	 
-	 vertexCount which is of type uint32_t is assigned vertices which is of type struct and notice we use static_cast
-	 to cast it to uint32_t because vulkan prefers fixed width types
-	 assert which is member of the header cassert is used to check for valid number of vertices
-	 in this case at least 3 for a triangle
-	 VkDeviceSize is of uint64 and we use to get the total memory required by multiplying the
-	 size of one vertex by how many we have
-	 we then call IkeDevice to create the Buffer with  five arguments via createBuffer(it creates a VkBuffer given size and usage flag, 
-	 it allocates VkDeviceMemory for that buffer and Binds the memory to the buffer ) we will also modify in the future
-	 we then create a pointer to a void data type
-	 we then call vkMapMemory() with 6 arguments (it maps GPU memory into the CPU's address space, after mapping, data points to a region of memory where we can write our vertex byte directly)
-	 we then call memcpy(that copies raw bytes from our vertices vector to the mapped GPU memory, vertices.data() returns a pointer to the vertex in the CPU array)
-	 we then call vkUnmapMemory() Unmaps the memory after copying telling vulkan that we are done, Since the memory is coherent, we do not need to flush it
-     this 3 actions will now make the vertex data to live in GPU-accessible memory
 
-	 */
 
 	void ikEngineModel::createVertexBuffers(const std::vector<Vertex>& vertices) {
 		vertexCount = static_cast<uint32_t>(vertices.size());
@@ -161,12 +131,9 @@ namespace ikE {
 	}
 
 
-	/* Note that before drawing we must bind the vertex buffer
-	   with the bind() that also takes the commandBuffer of type VkCommandBuffer 
-	   buffers[] is the array of buffers to bind and in this case it is the VertexBuffer of type VkBuffer as its argument
-	   offset[] is how far into each buffer to start reading in this case it is 0
-	   then we call vkCmdBindingVertexBuffers() with 5 arguments
-	   */
+
+
+
 	void ikEngineModel::bind(VkCommandBuffer commandBuffer) {
 		VkBuffer buffers[] = { vertexBuffer->getBuffer()};
 		VkDeviceSize offsets[] = { 0 };
@@ -178,8 +145,8 @@ namespace ikE {
 	   }
 	}
 
-	/* here we have the vector argument then ikEngineModel the class and Vertex the nested struct and getBindingDescription a member of the static member of the struct Vertex
-	   Notice that we did not use the static keyword here only in the hpp file */
+
+
 	std::vector<VkVertexInputBindingDescription>ikEngineModel::Vertex::getBindingDescriptions() {
 
 
